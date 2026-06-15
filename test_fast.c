cbba8487a0fbd284e19d138a8e268fe9ddc2d4e2
Yatsfr <marcoalfredodyatco1@gmail.com>
2026-06-15 13:01:22 +0000

    finish driver programs

---
+++ test_fast.c
@@ -0,0 +1,75 @@
+/**
+
+    >> Encode your group number (2 digits) and last names, first names and sections of group members.  Encode the purpose of this file.
+
+    Group #: 06  (2 digits)
+
+    DE GUZMAN, MAVRICK L.    S09
+    NONO, SHENELLE ANDREA G. S09
+    YATCO, MARCO ALFREDO D.  S09 
+
+    PURPOSE OF THIS FILE: to show an example of a C source file that follows the coding guidelines/instructions.
+
+    >> GENERAL INSTRUCTIONS: NON-COMPLIANCE WILL RESULT INTO AT LEAST A 10 POINT DEDUCTION!
+    1. Adhere with good programming style and practice (learned from CCPROG1 and CCPROG2).
+    2. Do NOT use global variables!  
+    3. Do NOT use goto statement!
+    4. Do NOT use return in a void function!
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// include your own stack header file
+#include "stack.h"
+#include "sort.c"
+#include "graham_slow.c"
+#define LIMIT 32768
+
+typedef char shortString[31]; //for items 30 characters
+
+int main()
+{
+    FILE *fp;
+    shortString inputFile, outputFile;
+    Stack S;
+    Point initialPoints[LIMIT];
+    Point hullPoints[LIMIT];
+    int i,j;
+    int n;
+    int m = 0;
+    int anchor;
+
+    printf("Enter your input filename: ");
+    scanf("%20s",inputFile);
+    printf("Enter your output filename: ");
+    scanf(" %20s",outputFile);
+
+    fp = fopen(inputFile, "r");
+
+    fscanf(fp, "%d", &n);
+    for (i = 0; i < n; i++) { 
+        fscanf(fp, "%lf %lf", &initialPoints[i].x, &initialPoints[i].y);  
+    }
+
+    fclose(fp);
+
+    S = grahamFast(initialPoints, n);
+
+    fp = fopen(outputFile, "w");
+    
+    while(!ISEMPTY(&S))
+    {
+        hullPoints[m++] = TOP(&S);
+        POP(&S);
+    }
+
+    fprintf(fp, "%d\n", m);
+    for (j = m - 1; j >= 0; j--)
+        fprintf(fp, "%.2f %.2f\n", hullPoints[j].x, hullPoints[j].y);
+
+    fclose(fp);
+
+    return 0;
+}
